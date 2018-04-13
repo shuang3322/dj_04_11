999c357ef9a40a8469f3a9df32c75da117be88ff
shuang3322 <shuang0528@hotmail.com>
2018-04-13 14:13:59 +0800

    finished sql

--- app01/models.py
+++ app01/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+class Business(models.Model):
+    # id
+    # id = models.AutoField(primary_key=True)
+    caption = models.CharField(max_length=32)
+    code = models.CharField(max_length=32,null=True,default="SA")
+
 # Create your models here.
+class Host(models.Model):
+    nid = models.AutoField(primary_key=True)
+    hostname = models.CharField(max_length=32, db_index=True)
+    ip = models.GenericIPAddressField(protocol="ipv4", db_index=True)
+    port = models.IntegerField()
+    # b = models.ForeignKey(to="Business",to_field= 'id',on_delete=models.SET_NULL,blank=True, null=True,db_constraint=True )
+    b = models.ForeignKey(to="Business",to_field= 'id',on_delete=models.CASCADE,)
+
+# 10
+class Application(models.Model):
+    name = models.CharField(max_length=32)
+
+# # 2
+#
+class HostToApp(models.Model):
+    # hobj = models.ForeignKey(to='Host', to_field='nid',on_delete=models.SET_NULL,blank=True, null=True)
+    hobj = models.ForeignKey(to='Host', to_field='nid',on_delete=models.CASCADE,)
+    # aobj = models.ForeignKey(to='Application', to_field='id',on_delete=models.SET_NULL,blank=True, null=True)
+    aobj = models.ForeignKey(to='Application', to_field='id',on_delete=models.CASCADE,)
+
+# HostToApp.objects.create(hobj_id=1,aobj_id=2)
--- dj_04_11/urls.py
+++ dj_04_11/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,re_path
 from app01 import views
+
 urlpatterns = [
-    path('', views.hall),
+    path('', views.app1),
+
 ]
+# urlpatterns = [
+#
+# ]
